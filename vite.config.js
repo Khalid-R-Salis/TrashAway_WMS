2955f93b500885ad2581e5e50afa71a6f3b2a6e6
Khalid Rabiu Salis <khalidrabiusalis@gmail.com>
2024-10-03 21:18:02 +0100

    reinstall package-lock.json&vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,14 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/", // Ensure this is set correctly for your deployment
+  base: "/", // For Vercel deployment
+  resolve: {
+    alias: {
+      // Add more aliases as needed
+      "@": "/src", // Alias to use '@' as a shorthand for '/src' folder
+    },
+  },
+  build: {
+    outDir: "dist", // Output directory for the build files
+  },
 });
