b26a57c8739b17396340db424e01260de164e657
Khalid Rabiu Salis <khalidrabiusalis@gmail.com>
2024-10-03 22:17:24 +0100

    remove About

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Index from "./pages/publicpages/index.jsx";
-import About from "./pages/publicpages/about"; // Correct import
-
-import Services from "./pages/publicpages/services.jsx";
-import Contact from "./pages/publicpages/contact.jsx";
-import Footer from "./components/Footer.jsx";
+import Index from "./pages/publicpages/index";
+// import About from "./pages/publicpages/about";
+import Services from "./pages/publicpages/services";
+import Contact from "./pages/publicpages/contact";
+import Footer from "./components/Footer";
 import "./index.css";
 
 function App() {
@@ -14,7 +13,7 @@ function App() {
         <div className="content">
           <Routes>
             <Route path="/" element={<Index />} />
-            <Route path="/about" element={<About />} />
+            {/* <Route path="/about" element={<About />} /> */}
             <Route path="/services" element={<Services />} />
             <Route path="/contact" element={<Contact />} />
           </Routes>
