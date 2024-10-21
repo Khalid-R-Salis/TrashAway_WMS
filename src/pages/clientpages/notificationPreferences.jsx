675d2420c87f96560d0460e5928d3ca074d8591c
Khalid Rabiu Salis <khalidrabiusalis@gmail.com>
2024-10-21 16:54:31 +0100

    Client notification, Client  Security

--- src/pages/clientpages/notificationPreferences.jsx
+++ src/pages/clientpages/notificationPreferences.jsx
@@ -1,6 +1,10 @@
 import React from "react";
+import { useState } from "react";
 
 const NotificationPreferences = () => {
+  const [emailNotifications, setEmailNotifications] = useState(false);
+  const [textMessage, setTextMessage] = useState(false);
+
   return (
     <div>
       <div className="p-6 bg-[#f1f4f8]/35 w-[80%] h-full">
@@ -15,15 +19,51 @@ const NotificationPreferences = () => {
           <h3 className="text-black font-medium my-4 font-Inter text-[16px]">
             General notifications
           </h3>
+          <div className="flex flex-col justify-start items-start gap-[20px]">
+            <div className="Email notifications flex justify-start items-start gap-2">
+              <label className="relative inline-flex items-center cursor-pointer">
+                <input
+                  type="checkbox"
+                  checked={emailNotifications}
+                  onChange={() => setEmailNotifications(!emailNotifications)}
+                  className="sr-only peer"
+                />
+                <div className="w-10 h-6 bg-gray-200 rounded-full peer dark:bg-[#E6E5EA] peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-[#83818E] after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-[#2CDB86]"></div>
+              </label>
 
-          <div className="">
-            <input type="radio" />
-            <h2>Email notifications</h2>
-            <p>
-              {/* We will send updates associated with your role to the email
-              address associated with your account. */}
-              <h1> THIS PAGE IS UNDER DEVLOPMENT</h1>
-            </p>
+              <div className="flex flex-col justify-start items-start gap-1 font-Inter">
+                <h1 className="text-[16px] font-[500] text-[#2B2A31]">
+                  Email notifications
+                </h1>
+                <p className="text-[14px] font-[400] text-[#83818E] w-[25rem]">
+                  We will send updates associated with your role to the email
+                  address associated with your account.
+                </p>
+              </div>
+            </div>
+
+            <div className="Email notifications flex justify-start items-start  gap-2">
+              <label className="relative inline-flex items-center cursor-pointer">
+                <input
+                  type="checkbox"
+                  checked={textMessage}
+                  onChange={() => setTextMessage(!textMessage)}
+                  className="sr-only peer"
+                  disabled
+                />
+                <div className="w-10 h-6 bg-gray-200 rounded-full peer dark:bg-[#E6E5EA] peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-[#83818E] after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-[#2CDB86]"></div>
+              </label>
+
+              <div className="flex flex-col justify-start items-start gap-1 font-Inter">
+                <h1 className="text-[16px] font-[500] text-[#2B2A31]">
+                  Text message
+                </h1>
+                <p className="text-[14px] font-[400] text-[#83818E] w-[25rem]">
+                  We will send you sensitive updates and activities regarding
+                  your role.
+                </p>
+              </div>
+            </div>
           </div>
         </div>
 
