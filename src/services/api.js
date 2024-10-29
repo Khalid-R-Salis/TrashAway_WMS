5d4dbd2f3115295df225d83d1b6d70c7be956b48
Khalid Rabiu Salis <khalidrabiusalis@gmail.com>
2024-10-29 14:59:58 +0100

    alter axions file

--- src/services/api.js
+++ src/services/api.js
@@ -1,25 +1,3 @@
-// import axios from "axios";
-
-// const api = axios.create({
-//   baseURL: "https://waste-mangement-backend-j7t0.onrender.com/api",
-//   headers: {
-//     "Content-Type": "application/json",
-//   },
-// });
-
-// api.interceptors.request.use(
-//   (config) => {
-//     const token = localStorage.getItem("token");
-//     if (token) {
-//       config.headers.Authorization = `Bearer ${token}`;
-//     }
-//     return config;
-//   },
-//   (error) => Promise.reject(error)
-// );
-
-// export default api;
-
 import axios from "axios";
 
 const api = axios.create({
